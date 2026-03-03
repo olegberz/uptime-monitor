d275e4a85396860b32d5922505bc282ba9f26b87
olegberz <74153652+olegberz@users.noreply.github.com>
2026-03-03 11:26:02 +0200

    Initial commit

---
+++ src/main/java/com/monitoring/websitemonitoring/WebSiteMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.monitoring.websitemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSiteMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSiteMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monitoring/websitemonitoring/WebSiteMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoring.websitemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
