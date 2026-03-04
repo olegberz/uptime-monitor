2a316a5c2746f26a6d9748673da3de7fd96d0229
olegberz <74153652+olegberz@users.noreply.github.com>
2026-03-04 09:09:34 +0200

    user rest controller created

--- src/main/java/com/monitoring/websitemonitoring/controller/UserRestController.java
+++ src/main/java/com/monitoring/websitemonitoring/controller/UserRestController.java
@@ -1,10 +1,16 @@
 package com.monitoring.websitemonitoring.controller;
 
+import com.monitoring.websitemonitoring.entity.User;
 import com.monitoring.websitemonitoring.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
-@RequestMapping("/website")
+@RequestMapping("/api/users")
 public class UserRestController {
     private final UserService userService;
 
@@ -12,4 +18,29 @@ public class UserRestController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        return ResponseEntity.ok(userService.findUserById(id));
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<User> createUser(@PathVariable Long id, @RequestBody User user) {
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return ResponseEntity.ok(userService.updateUser(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
